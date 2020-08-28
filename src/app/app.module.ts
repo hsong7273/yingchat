d5052d656919d165f6bdf5def34a958ebc51dded
hsong7273 <song.william7273@gmail.com>
2020-08-28 03:05:46 -0400

    added Firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,24 @@ import { RouterModule } from '@angular/router';
 
 import { FormsModule } from '@angular/forms';
 
+// FireBase/FireStore Libraries
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
+//FireBase Credentials
+const firebaseConfig = {
+  apiKey: "AIzaSyADQUMBfb2a0pM642uFzXyn2F38yccXu9U",
+  authDomain: "easychat-f2728.firebaseapp.com",
+  databaseURL: "https://easychat-f2728.firebaseio.com",
+  projectId: "easychat-f2728",
+  storageBucket: "easychat-f2728.appspot.com",
+  messagingSenderId: "839409639110",
+  appId: "1:839409639110:web:6eb6102eef8917f12141c7",
+  measurementId: "G-W1L5TXZ7XF"
+};
+
 import { AppComponent } from './app.component';
 import { ChatComponent } from './chat/chat.component';
 
