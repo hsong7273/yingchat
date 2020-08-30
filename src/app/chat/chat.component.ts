88744a473bb21cf6f2408cb874f8c3321a0560fc
hsong7273 <song.william7273@gmail.com>
2020-08-30 16:44:07 -0400

    writing messages to database

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from "@angular/core";
-import { AngularFireDatabase } from '@angular/fire/database';
+import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
 import { Observable } from 'rxjs';
-
+import { Message } from './message';
 
 @Component({
   selector: "app-chat",
@@ -9,10 +9,20 @@ import { Observable } from 'rxjs';
   styleUrls: ["./chat.component.css"]
 })
 export class ChatComponent implements OnInit {
+  newMessage: Message;
+  itemsRef: AngularFireList<any>;
   messages: Observable<any[]>;
   constructor(db: AngularFireDatabase) {
+    this.itemsRef = db.list('messages');
     this.messages = db.list('messages').valueChanges();
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.newMessage = new Message();
+  }
+
+  addItem() {
+    this.newMessage.date = String(new Date());
+    this.itemsRef.push( this.newMessage );
+  }
 }
